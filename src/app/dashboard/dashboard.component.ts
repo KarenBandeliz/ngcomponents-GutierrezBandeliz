5f5761fbe6100d2ac7d318bcb2d0f9621f8b2ade
KarenBandeliz <96149781+KarenBandeliz@users.noreply.github.com>
2022-06-29 21:19:35 -0500

    lista alumnos

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  show:boolean = false;
+  showtitle:string = "";
+  Lista = [
+    {id: 1, nombre:'Karen', calificacion: 10},
+    {id: 2, nombre:'José', calificacion: 8},
+    {id: 5, nombre:'Omar', calificacion: 9},
+    {id: 3, nombre:'Iván', calificacion: 7},
+    {id: 4, nombre:'Giselle', calificacion: 10},
+    {id: 5, nombre:'Ramsés', calificacion: 8},
+    {id: 6, nombre:'Raúl', calificacion: 5},
+    {id: 7, nombre:'Nathaly', calificacion: 6},
+    {id: 8, nombre:'Josué', calificacion: 5},
+    {id: 9, nombre:'Amelie', calificacion: 10},
+    {id: 10, nombre:'Carlos', calificacion: 4}
 
+  ];
+
+  lista = this.Lista;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
+  }
+  Aprobado(){
+    this.show = true;
+    this.showtitle = "Aprobado, felicidades"
+
+  }
+  Reprobado(){
+    this.showtitle = "Reprobado, estudia más"
+    this.show = false;
   }
 
 }
